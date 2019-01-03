51a623379acdbb599428709b6965dc19f77be28e
jeff <jeff.hanna@gmail.com>
2019-01-04 00:47:11 +0100

    b

--- vec_play.py
+++ vec_play.py
@@ -8,11 +8,12 @@ import matplotlib.pyplot as plt
 plt.ion()
 
 class Bilder:
-    def __init__(self,rects,z_cuda,netGx,axis,background):
+    def __init__(self,rects,z_cuda,netGx,axis,background,deriv_imshow=None):
         self.rects = rects
         self.z_cuda = z_cuda
         self.netGx = netGx
         self.axis = axis
+        self.deriv_imshow = deriv_imshow
         x_hat = netGx(z_cuda).detach()
         plt.sca(self.axis)
         self.imshow = plt.imshow(x_hat[0,0,].cpu().numpy().astype(np.float32),
@@ -21,8 +22,11 @@ class Bilder:
         z = np.array([r.get_height() for r in self.rects])
         self.z_cuda[0,:,0,0].copy_(torch.from_numpy(z))
         x_hat = self.netGx(z_cuda).detach()
+        x_hat_nump = x_hat[0,0,].cpu().numpy().astype(np.float32)
+        if self.deriv_imshow:
+            self.deriv_imshow.set_data(abs(self.imshow.get_array()-x_hat_nump))
         plt.sca(self.axis)
-        self.imshow.set_data(x_hat[0,0,].cpu().numpy().astype(np.float32))
+        self.imshow.set_data(x_hat_nump)
 
 class DragBars:
     def __init__(self,rects,ids,bilder,axis,background):
@@ -60,11 +64,24 @@ class DragBars:
             direction = -1
         elif event.key == "shift+down":
             direction = -10
+        elif event.key == "0":
+            direction = 0
+        elif event.key == "r":
+            for r_idx,r in enumerate(self.rects):
+                self.ons[r_idx] = False
+                r.set_color("blue")
+                r.set_height(np.random.normal(loc=0,scale=1))
+            self.draw()
+            self.bilder.draw()
+            return
         else:
             return
         for r_idx,r in enumerate(self.rects):
             if self.ons[r_idx]:
                 r.set_height(r.get_height()+self.incr*direction)
+            else:
+                if not direction:
+                    r.set_height(0)
         self.draw()
         self.bilder.draw()
 
@@ -78,6 +95,7 @@ class DragBars:
 net_dir = "/home/jeff/deepeeg/all_b400_z28_res/"
 netGxFile = net_dir+"netGx_epoch_0_1.pth"
 vecfile = net_dir+"93_A_hand_5ms_ica-raw.fif_Z"
+deriv_plot = True
 
 z_cuda = torch.FloatTensor(1,28,1,1).cuda()
 netGx = models.Generator()
@@ -94,7 +112,14 @@ backgrounds = [fig.canvas.copy_from_bbox(ax.bbox) for ax in axes]
 z = np.random.normal(loc=0,scale=1,size=28)
 plt.sca(axes[1])
 barplot = plt.bar(np.array(range(len(z))),z,color="blue")
-bilder = Bilder(barplot.patches,z_cuda,netGx,axes[0],backgrounds[0])
+plt.ylim((-3,3))
+if deriv_plot:
+    deriv_fig = plt.figure()
+    deriv_imshow = plt.imshow(np.zeros((64,64)),vmin=0,vmax=.05)
+    plt.axis("off")
+    bilder = Bilder(barplot.patches,z_cuda,netGx,axes[0],backgrounds[0],deriv_imshow=deriv_imshow)
+else:
+    bilder = Bilder(barplot.patches,z_cuda,netGx,axes[0],backgrounds[0],deriv_imshow=None)
 bilder.draw()
 db = DragBars(barplot.patches,[str(x) for x in list(range(28))],
 bilder,axes[1],backgrounds[1])
